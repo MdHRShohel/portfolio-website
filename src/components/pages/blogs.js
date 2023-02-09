46c2615e448bdb76b6f2ba5cba8a6ef583c32d5b
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2023-02-10 00:50:26 +0600

    blog update

--- src/components/pages/blogs.js
+++ src/components/pages/blogs.js
@@ -18,6 +18,7 @@ class Blogs extends Component {
         <NavBar page="blogs" />
         <div className="projects-container">
           <h2>Blogs</h2>
+          {/* 1st Blog */}
           <div className="project">
             <picture className="project-image">
               <img
@@ -43,6 +44,32 @@ class Blogs extends Component {
               </p>
             </div>
           </div>
+          <div className="project">
+            <picture className="project-image">
+              <img
+                src="https://miro.medium.com/max/720/1*kxBdslclglg4zgCw0NMIIA.webp"
+                alt="New Zealand Police Logo"
+                loading="lazy"
+              />
+            </picture>
+            <div className="project-text">
+              <h3>যাপিত জীবনঃ মানবতার বাটখারা।</h3>
+              <span>
+                <strong>Date: </strong>১৬ ই এপ্রিল, ২০২২ বিকাল ৩:১৭
+              </span>
+              <br />
+              <br />
+              <p>
+                পৃথিবীর উন্নত মানবতার জনৈক দেশে একটি ধর্মের পবিত্র বইকে পুড়িয়ে
+                ফেলার জন্য দেশটির একজন রাজনীতিবিদ কর্মসূচীকে ঘোষনা করলে তা
+                অনুমোদন করা হয় এবং যখন তার প্রতিবাদে উক্ত ধর্মের লোকজন রাস্তায়
+                নেমে ভাংচুর করে তখন বলা হয় সহিংস বিক্ষোভ। আধুনিক বিশ্ব ও তাদের
+                সংবাদপত্রের নীতি নৈতিকতা খুবই ভালো। এই ধরনের নীতিমালা অন্য
+                ধর্মের কেউ অনুসরণ করলে তারা হয়ে যায় সন্ত্রাসী।
+              </p>
+            </div>
+          </div>
+          {/* 3rd Blog */}
           <div className="project">
             <picture className="project-image">
               <img
